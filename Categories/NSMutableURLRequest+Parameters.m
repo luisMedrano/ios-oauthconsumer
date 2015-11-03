7ca96e63f33259badd8a50955a5ccd091f856d64
Jens Alfke <jens@couchbase.com>
2015-11-03 12:39:49 -0800

    Fixed an assertion-failure crash parsing request parameters

--- Categories/NSMutableURLRequest+Parameters.m
+++ Categories/NSMutableURLRequest+Parameters.m
@@ -53,6 +53,8 @@ static NSString *Boundary = @"-----------------------------------0xCoCoaouTHeBou
     
     for (NSString *encodedPair in encodedParameterPairs) {
         NSArray *encodedPairElements = [encodedPair componentsSeparatedByString:@"="];
+        if (encodedPairElements.count != 2)
+            return nil;
         OARequestParameter *parameter = [[OARequestParameter alloc] initWithName:[[encodedPairElements objectAtIndex:0] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]
                                                                            value:[[encodedPairElements objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
         [requestParameters addObject:parameter];
